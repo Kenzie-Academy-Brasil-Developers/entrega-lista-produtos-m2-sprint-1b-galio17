f4bd991b7ddaf02dde0af9a6755eb34ff229a089
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-05-25 08:45:42 -0300

    Feat: Creat function to list products

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
+function listFoods (productsData) {
+    const productList = document.querySelector('ul');
 
+    productList.innerHTML = '';    
+
+    productsData.forEach((product) => {
+        const newProduct = createProductCard(product);
+        productList.append(newProduct);
+    });
+}
+
+listFoods(produtos);
