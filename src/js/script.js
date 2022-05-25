b75d84126ef0dfd3e32d2dc707e14557c7a4311a
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-05-25 08:46:45 -0300

    Feat: Creat function to create product's card

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,17 @@ function listFoods (productsData) {
     });
 }
 
+function createProductCard (product) {
+    const productCard = document.createElement('li');
+    const productImage = createProductImage(product);
+    const productTitle = createProductInfo('h3', product.nome);
+    const productPrice = createProductInfo('p', `R$ ${product.preco.toFixed(2)}`);
+    const productSection = createProductInfo('span', `${product.secao}`);
+
+    productCard.append(productImage, productTitle, productPrice, productSection);
+
+    return productCard;
+}
+
+
 listFoods(produtos);
